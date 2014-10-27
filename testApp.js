27dadb729f03cdb17fa26b029ece454a60600ede
graitapon <graitapon.t@yru.ac.th>
2014-10-27 12:04:09 +0700

    test modal

--- testApp.js
+++ testApp.js
@@ -11,4 +11,29 @@ angular.module('testApp', [])
 	{ image: '3778-11.jpg' },
 	{ image: '3787-11.jpg' }
 	];
-});
+})
+.controller('ModalDemoCtrl', function ($scope, $modal, $log) {
+
+  $scope.items = ['item1', 'item2', 'item3'];
+
+  $scope.open = function (size) {
+
+    var modalInstance = $modal.open({
+      templateUrl: '/myModalContent.html',
+      controller: 'ModalInstanceCtrl',
+      size: size,
+      resolve: {
+        items: function () {
+          return $scope.items;
+        }
+      }
+    });
+
+    modalInstance.result.then(function (selectedItem) {
+      $scope.selected = selectedItem;
+    }, function () {
+      $log.info('Modal dismissed at: ' + new Date());
+    });
+  };
+})
+;
