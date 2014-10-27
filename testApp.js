f05ce31dbd1583c9cfc411b8459676c058854004
graitapon <graitapon.t@yru.ac.th>
2014-10-27 11:46:22 +0700

    test ng-repeat

--- testApp.js
+++ testApp.js
@@ -1,8 +1,14 @@
 angular.module('testApp', [])
-.controller('CarouselDemoCtrl', function ($scope) {
-  $scope.myInterval = 2000;
-  $scope.slides = [
-  { image: '3778-11.jpg' },
-  { image: '3787-11.jpg' }
-  ];
+.controller('CarouselDemoCtrl', function($scope) {
+	
+	$scope.data = [
+	{ image: '3778-11.jpg' },
+	{ image: '3787-11.jpg' }
+	];
+	
+	$scope.myInterval = 2000;
+	$scope.slides = [
+	{ image: '3778-11.jpg' },
+	{ image: '3787-11.jpg' }
+	];
 });
