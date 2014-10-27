eb19b2607edc63e8f659178bfdad268a44dd84a0
graitapon <graitapon.t@yru.ac.th>
2014-10-27 11:20:09 +0700

    test carousel

---
+++ js/carousel.js
@@ -0,0 +1,8 @@
+angular.module('CarouselApp', [])
+.controller('CarouselDemoCtrl', function ($scope) {
+  $scope.myInterval = 2000;
+  $scope.slides = [
+  { image: '3778-11.jpg' },
+  { image: '3787-11.jpg' }
+  ];
+});
